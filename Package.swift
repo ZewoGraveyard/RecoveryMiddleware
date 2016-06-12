d9ad705e5d01bf3b2b0055b1c4da029242fc6af1
Dan Appel <dan.appel00@gmail.com>
2016-06-11 18:31:28 -0700

    Bump dependencies and update to 05-09

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "RecoveryMiddleware",
     dependencies: [
-        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 8),
     ]
 )
